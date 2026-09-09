7376597a0194610754928f7d0adeda50e8d106f4
Jakhongir <jumaevjakhongir6801@gmail.com>
2026-09-09 10:38:10 +0900

    feat:Reja lohiyamzining yakuniy bosqich loguc

--- public/browser.js
+++ public/browser.js
@@ -48,7 +48,31 @@ document.addEventListener("click", function(e){
     }
 
     //edit oper
-    if(e.target.classList.contains("edit-me")){
-        alert("Siz edit tugmasini bosdingiz");
-    }
+  if(e.target.classList.contains("edit-me")){
+    let userInput = prompt("O'zgartirishni kiriting", 
+  e.target.parentElement.parentElement.querySelector(".item-text").innerHTML);
+  if(userInput){
+    axios.post("edit_item",{
+    id: e.target.getAttribute("data-id"),
+    new_input:userInput,
+ }).then(response => {
+    console.log(response.data);
+    e.target.parentElement.parentElement.querySelector(".item-text").innerHTML = userInput;
+ })
+ .catch(err =>{
+    conlsole.log("Iltimos, qayta urinib ko'ring");
+ })
+  }
+}
+
 });
+
+document.getElementById("clean_all").addEventListener("click", function(){
+    axios.post("/delete-all", {delete_all: true}).then(response=>{
+        alert(response.data.state);
+        document.location.reload();
+    });
+});
+
+
+
